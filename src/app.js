7131b09e6c62cb380386964d85e57844f45ad58b
GermanShtey <gvshteynardt@gmail.com>
2020-12-17 19:52:41 +0300

    feat: добавить react select

--- src/app.js
+++ src/app.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
 import articles from './fixtures'
 import ArticleList from './components/ArticleList'
+import Select from 'react-select'
 
 class App extends Component {
+  state = {
+    openItem: null
+  }
+
   render() {
     return (
       <div>
+        <Select
+          options={this.options}
+          value={this.state.openItem}
+          onChange={this.handleSelect}
+        />
         <ArticleList articles={articles} />
       </div>
     )
   }
+
+  get options() {
+    return articles.map((article) => ({
+      label: article.title,
+      value: article.id
+    }))
+  }
+
+  handleSelect = (openItem) => this.setState({ openItem: openItem })
 }
 
 export default App
