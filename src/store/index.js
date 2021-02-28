3bbcaabafd81e7b871dbcff1ae2ecae82e9a1e82
GermanShtey <gvshteynardt@gmail.com>
2021-02-28 21:39:29 +0300

    added redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,21 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import reducer from '../reducer/index';
 import logger from "../middlewares/logger";
 
-const enhancer = applyMiddleware(logger);
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(logger),
+  // other store enhancers if any
+);
 const store = createStore(reducer, enhancer);
+
 //dev only!!
 window.store = store;
 
